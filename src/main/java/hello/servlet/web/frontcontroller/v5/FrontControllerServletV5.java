dea1b188675aba901094f82d561e5372e0aeb8c3
bigduk88 <hangdeuk@naver.com>
2021-07-13 10:40:00 +0900

    [2021.07.13] Adapter v5(v4)

--- src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -8,6 +8,7 @@ import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
 import hello.servlet.web.frontcontroller.v4.ControllerV4;
 import hello.servlet.web.frontcontroller.v5.adapter.ContorllerV3HandlerAdapter;
+import hello.servlet.web.frontcontroller.v5.adapter.ContorllerV4HandlerAdapter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -35,16 +36,21 @@ public class FrontControllerServletV5 extends HttpServlet {
         handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV3());
     }
 
     private void initHandlerAdapters() {
         handlerAdapters.add(new ContorllerV3HandlerAdapter());
+        handlerAdapters.add(new ContorllerV4HandlerAdapter());
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        final Object handler = getHandler(request);
+        Object handler = getHandler(request);
 
         if (handler == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
